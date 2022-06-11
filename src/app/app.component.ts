f79d78b3d2964e9374cb6b66f58a248a0b86c142
Khoa TRAN <khoatrandinh94@gmail.com>
2022-06-11 19:53:17 +0200

    kinda work. TODO: highlight sorted part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  // https://stackoverflow.com/questions/64485771/visualising-bubble-sort-using-chart-js
   bubbleSort(chart: any): void {
     let labels = chart.data.labels;
     let data = chart.data.datasets[0].data;
@@ -21,10 +22,16 @@ export class AppComponent implements OnInit {
         if (data[i] > data[i + 1]) {
           this.swap(labels, i);
           this.swap(data, i);
-          this.swap(colors, i);
-          timeout += 25;
+          // this.swap(colors, i);
+          colors[i] = '#3366E6';
+          colors[i+1] = '#3366E6';
+          timeout += 5;
           this.updateChartDelayed(chart, labels.slice(0), data.slice(0), colors.slice(0), timeout);
+          timeout += 10;
           swapped = true;
+          colors[i] = '#FFB399';
+          colors[i+1] = '#FFB399';
+          this.updateChartDelayed(chart, labels.slice(0), data.slice(0), colors.slice(0), timeout);
         }
       }
     } while (swapped);
@@ -41,7 +48,7 @@ export class AppComponent implements OnInit {
       chart.data.labels = labels;
       chart.data.datasets[0].data = data;
       chart.data.datasets[0].backgroundColor = colors;
-      chart.update();
+      chart.update('none');
     }, timeout);
   }
 
@@ -67,12 +74,15 @@ export class AppComponent implements OnInit {
         labels: this.labels,
         datasets: [{
           data: this.labels.map(() => Math.random()),
-          backgroundColor: ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D', '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399', '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933', '#FF4D4D'],
+          // backgroundColor: ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D', '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399', '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933', '#FF4D4D'],
+          backgroundColor: Array(26).fill('#FFB399'),
           borderWidth: 1
         }]
+      },
+      options: {
+        animations: false
       }
     });
-
-    setTimeout(() => this.bubbleSort(myChart), 1000);
+    this.bubbleSort(myChart);
   }
 }
