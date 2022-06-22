5e6ee331f8cd1fe0c0328b14194ae1fd7a1cc425
Khoa TRAN <khoatrandinh94@gmail.com>
2022-06-23 00:02:14 +0200

    Refactor + Bootstrap. TODO: fix dropdown button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SortComponent } from './sort/sort.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'sort' },
+  {path: 'sort', component: SortComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
